cc29fc934bb4c2562f1b1c9a16b4fb6ae3004aa1
sunn1day <0v4rl0r5@gmail.com>
2022-10-08 20:42:53 +0200

    setup.sh fix

--- setup.sh
+++ setup.sh
@@ -11,15 +11,15 @@ if [ -z "$PYTHON_VER" ]; then
   exit 1
 fi
 
-if [ -z "`which unzip`" ]; then
-  echo "[!] 'unzip' is no present, install it before continuing"
-  exit 1
-fi
-
-if [ -z "`which cmake`" ]; then
-  echo "[!] 'cmake' is no present, install it before continuing"
-  exit 1
-fi
+#if [ -z "`which unzip`" ]; then
+#  echo "[!] 'unzip' is no present, install it before continuing"
+#  exit 1
+#fi
+#
+#if [ -z "`which cmake`" ]; then
+#  echo "[!] 'cmake' is no present, install it before continuing"
+#  exit 1
+#fi
 
 if [ -z "`which readelf`" ]; then
   echo "[!] 'readelf' is no present, install it before continuing"
@@ -42,8 +42,14 @@ if [ -z "`which radare2`" ]; then
   exit 1
 fi
 
+if [ -z "`which virtualenv`" ]; then
+  echo "[!] Install: 'sudo apt-get install ${PYTHON_VER}-distutils ${PYTHON_VER}-setuptools virtualenv'"
+  echo ""
+  exit 1
+fi
 
-if [ $1 == "--clean" ]; then
+
+if [ "$1" == "--clean" ]; then
   rm -rf keystone keystone_repo LIEF lief.so r2pipe
   exit 1
 fi
@@ -51,47 +57,75 @@ fi
 ###
 ## 1. Instal keystone python module
 #
-if [ ! -d keystone_repo ]; then
-  echo "[!] 'keystone' is no present ... installing it now (only on current folder)"
-
-  wget https://github.com/keystone-engine/keystone/archive/refs/tags/${KEYSTONE_VER}.zip
-  unzip ${KEYSTONE_VER}.zip
-  rm ${KEYSTONE_VER}.zip
-
-  mv keystone-${KEYSTONE_VER} keystone_repo
-  cd keystone_repo/bindings/python
-
-  $PYTHON_VER setup.py build
-  cd ../../..
-  mv keystone_repo/bindings/python/keystone .
-fi
+#if [ ! -d keystone_repo ]; then
+#  echo "[!] 'keystone' is no present ... installing it now (only on current folder)"
+#
+#  wget https://github.com/keystone-engine/keystone/archive/refs/tags/${KEYSTONE_VER}.zip
+#  unzip ${KEYSTONE_VER}.zip
+#  rm ${KEYSTONE_VER}.zip
+#
+#  mv keystone-${KEYSTONE_VER} keystone_repo
+#  cd keystone_repo/bindings/python
+#
+#  $PYTHON_VER setup.py build
+#  cd ../../..
+#  mv keystone_repo/bindings/python/keystone .
+#fi
 
 
 ###
 ## 2. Check LIEF
 #
-if [ ! -f lief.so ]; then
-  echo "[!] 'LIEF' is no present ... installing it now (only on current folder)"
+#if [ ! -f lief.so ]; then
+#  echo "[!] 'LIEF' is no present ... installing it now (only on current folder)"
+#
+#  wget https://github.com/lief-project/LIEF/archive/refs/tags/${LIEF_VER}.zip
+#  unzip ${LIEF_VER}.zip
+#  rm ${LIEF_VER}.zip
+#
+#  mv LIEF-${LIEF_VER} LIEF
+#  cd LIEF
+#  $PYTHON_VER setup.py build
+#
+#  if [ $? != 0 ]; then
+#    echo "[!] error while building LIEF module"
+#    echo " \---> Run 'sudo apt-get install ${PYTHON_VER}-distutils ${PYTHON_VER}-setuptools'"
+#    exit
+#  fi
+#
+#  cd ..
+#  mv LIEF/build/lief.so .
+#
+#fi
 
-  wget https://github.com/lief-project/LIEF/archive/refs/tags/${LIEF_VER}.zip
-  unzip ${LIEF_VER}.zip
-  rm ${LIEF_VER}.zip
 
-  mv LIEF-${LIEF_VER} LIEF
-  cd LIEF
-  $PYTHON_VER setup.py build
+if [ ! -d keystone ]; then
+  echo "[!] 'keystone' is no present ... installing it now (only on current folder)"
+  rm -rf object_virtualenv; mkdir object_virtualenv && virtualenv --python=$PYTHON_VER object_virtualenv &&\
+  source object_virtualenv/bin/activate &&\
+  pip install keystone &&\
+  deactivate
 
-  if [ $? != 0 ]; then
-    echo "[!] error while building LIEF module"
-    echo " \---> Run 'sudo apt-get install ${PYTHON_VER}-distutils ${PYTHON_VER}-setuptools'"
-    exit
-  fi
+  find -name keystone -type d | while read x; do
+    mv "$x" keystone
+  done
 
-  cd ..
-  mv LIEF/build/lief.so .
-
+  rm -rf object_virtualenv
 fi
 
+if [ ! -f lief.so ]; then
+  echo "[!] 'LIEF' is no present ... installing it now (only on current folder)"
+  rm -rf object_virtualenv; mkdir object_virtualenv && virtualenv --python=$PYTHON_VER object_virtualenv &&\
+  source object_virtualenv/bin/activate &&\
+  pip install lief &&\
+  deactivate
+
+  find -name "lief*.so" -type f | while read x; do
+    mv "$x" lief.so
+  done
+
+  rm -rf object_virtualenv
+fi
 
 if [ ! -d r2pipe ]; then
   echo "[!] 'r2pipe' is no present ... installing it now (only on current folder)"
@@ -106,7 +140,6 @@ if [ ! -d r2pipe ]; then
 
   rm -rf object_virtualenv
 fi
-
 
 echo "[+] Done!"
 
