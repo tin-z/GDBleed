9f47057a17737435321afbc3bbc68191f148d374
tin-z <altin.thartori.0@gmail.com>
2023-07-18 01:09:15 +0200

    checkpoint

--- core/constants.py
+++ core/constants.py
@@ -41,3 +41,91 @@ special_regs = {
 
 
 
+
+# ref https://ablconnect.harvard.edu/files/ablconnect/files/mips_instruction_set.pdf
+mips_branch_type = [
+    "beq" ,\
+    "bne" ,\
+    "blez" ,\
+    "bgtz" ,\
+    "bltz" ,\
+    "bgez" ,\
+    "bltzal" ,\
+    "bgezal" ,\
+]
+
+
+# ref https://www.slideserve.com/elina/68000-addressing-modes
+#     https://www.intel.com/content/www/us/en/docs/programmable/683620/current/bgtu.html
+arm32_branch_type = [
+    "beq" ,\
+    "bne" ,\
+    "bpl" ,\
+    "bmi" ,\
+    "bcc" ,\
+    "blo" ,\
+    "bcs" ,\
+    "bhs" ,\
+    "bvc" ,\
+    "bvs" ,\
+    "bge" ,\
+    "bgt" ,\
+    "ble" ,\
+    "blt" ,\
+    "bhi" ,\
+    "bls" ,\
+    "bgtu" ,\
+    "bltu" ,\
+    "bgeu" ,\
+    "bleu" ,\
+    "beqz" ,\
+    "bnez" ,\
+    "blez" ,\
+    "bgez" ,\
+    "bltz" ,\
+    "bgtz" ,\
+]
+
+# https://www.felixcloutier.com/x86/jcc
+x86_64_branch_type = [
+    'jnz' ,\
+    'jpo' ,\
+    'jnl' ,\
+    'jpe' ,\
+    'jnle' ,\
+    'jb' ,\
+    'jng' ,\
+    'jcxz' ,\
+    'jz' ,\
+    'jc' ,\
+    'jecxz' ,\
+    'js' ,\
+    'jle' ,\
+    'jrcxz' ,\
+    'je' ,\
+    'jo' ,\
+    'jnge' ,\
+    'jno' ,\
+    'jge' ,\
+    'ja' ,\
+    'jnae' ,\
+    'jnbe' ,\
+    'jna' ,\
+    'jl' ,\
+    'jne' ,\
+    'jns' ,\
+    'jnp' ,\
+    'jae' ,\
+    'jp' ,\
+    'jnc' ,\
+    'jbe' ,\
+    'jg' ,\
+    'jnb' ,\
+]
+
+JCC_types = {
+  "arm" : arm32_branch_type ,\
+  "x86-64" : x86_64_branch_type ,\
+  "mips" : mips_branch_type ,\
+}
+
